1f132c561c686cfd968339ba2439a1e9c681347f
Miha Markovic <miha0552@gmail.com>
2016-11-17 16:07:08 +0100

    frizerski salon mirko z GAE

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        return self.response.write('Hello SmartNinja!')
+        f = open("index.html")
+        return self.response.write(f.read())
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
